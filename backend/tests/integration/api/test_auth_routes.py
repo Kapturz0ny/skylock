5f7bfed62f844ab172dbad5ee6eba2e2cb91312c
Andrzej Pultyn <01178581@pw.edu.pl>
2025-04-09 14:25:58 +0200

    Reverted changes to integration test

--- backend/tests/integration/api/test_auth_routes.py
+++ backend/tests/integration/api/test_auth_routes.py
@@ -1,43 +1,6 @@
-from unittest.mock import MagicMock, ANY, patch
-from skylock.service.user_service import UserService
-
-@patch("skylock.service.user_service.send_mail")
-def test_register_success(mock_send_mail):
-    mock_user_repository = MagicMock()
-    mock_user_repository.get_by_username.return_value = None
-    mock_user_repository.get_by_email.return_value = None
-
-    mock_redis = MagicMock()
-
-    user_service = UserService(
-        user_repository=mock_user_repository,
-        redis_mem=mock_redis
-    )
-
-    username = "testuser"
-    password = "securepassword"
-    email = "testuser@example.com"
-
-    user_service.register_user(username, password, email)
-
-    user_service.redis_mem.setex.assert_called_once_with(
-        f"2fa:{username}",
-        user_service.TOKEN_LIFE + 5,
-        ANY
-    )
-
-    mock_send_mail.assert_called_once_with(
-        email,
-        "Complete you registration to Skylock!",
-        ANY
-    )
-
-
-
 def test_register_user_success(client):
     username = "testuser"
     password = "securepassword"
-    email = "test@example.com"
 
     response = client.post("/auth/register", json={"username": username, "password": password, "email": email})
 
